4e5a032ca2344730cfd74fea535660eaf6819008
songshenglin <13639120050@163.com>
2024-08-21 09:06:44 +0800

    主函数和测试优化

--- A04_imooc_pytorch/A04_pytorch/02_demo_train.py
+++ A04_imooc_pytorch/A04_pytorch/02_demo_train.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from tqdm import tqdm
 
 '''
 训练函数的模版:
@@ -27,7 +28,6 @@ for x, y in train_dataLoader:
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
-
 print("test_dataLoader-come")
 for x, y in test_dataLoader:
     # Shape of X [N, C, H, W]: torch.Size([64, 1, 28, 28])
@@ -37,6 +37,7 @@ for x, y in test_dataLoader:
 
 # 模型
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 
 class NeuralNetWork(nn.Module):
     def __init__(self):
@@ -63,39 +64,56 @@ model = NeuralNetWork().to(device)
 print(model)
 
 # 损失函数，CrossEntropyLoss=交叉熵损失函数，常用语分类任务
+# 如果 loss_fn 是交叉熵损失（例如 torch.nn.CrossEntropyLoss），它通常会产生较大的数值，尤其是在模型刚开始训练时，分类错误较多的情况下。
+# 如果 loss_fn 是均方误差损失（例如 torch.nn.MSELoss），并且输出和目标值的差距较大时，损失值也会很大。
 loss_fn = nn.CrossEntropyLoss()
 
 # 优化器，用于更新模型参数，lr=1e-3是学习率
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 
-# 定义训练函数
+# 定义训练函数-优化
 def train_fun(dataLoader, model, loss_fn, optimizer):
-    size = len(dataLoader.dataset)
-    # 模型调到训练模式
+    data_total_size = len(dataLoader.dataset)
+    data_batch = len(dataLoader)
+
     model.train()
-    '''
-        X:图片
-        y:标签真实值
-        batch:X遍历的下标
-    '''
-    for batch, (x, y) in enumerate(dataLoader):
+
+    # 总的损失
+    loss_total = 0
+    # 预测正确的个数
+    correct = 0
+    for x, y in tqdm(dataLoader):
         x, y = x.to(device), y.to(device)
 
-        # 将输入的x进行模型训练
+        '''
+        预测结果:tensor([[ 2.0645e-02, -1.0214e-01,  7.6782e-02, -1.2014e-02,  1.3320e-01,
+         -1.8308e-01,  1.6507e-02, -1.1457e-01,  2.1594e-01,  1.5671e-02],
+        [-3.9951e-03, -2.3991e-02,  2.6100e-02,  1.5857e-02,  3.9585e-02,
+        '''
         pred = model(x)
-        # 计算预测结果和输出结果相差多少
-        loss = loss_fn(pred, y)
-        # 反向传播和优化
-        loss.backward()  # 反向传播
-        optimizer.step()  # 更新参数
-        optimizer.zero_grad()  # 清空梯度
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(x)
-            # loss:{loss:>7f} 表示损失值 loss 会被格式化为浮点数，保留 7 位宽度，不足的部分会在左边填充空格。
-            # :f 表示浮点数格式，> 表示右对齐，7 表示总共占用 7 位字符宽度。
-            print(f"loss:{loss:>7f} [{current:>5d}/{size:>5d}]")
+        '''
+        loss函数结果:2.235142230987549
+        '''
+        loss = loss_fn(pred, y)
+        loss_total += loss.item()
+
+        '''
+        pred.argmax(1) == y的结果:tensor([ True, False, False, False, False, False,  True, False,  True, False,
+         True,  True, False, False,  True, False,  True, False, False, False,
+        False, False, False, False, False, False, False, False,  True, False,
+        '''
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+    loss_avg = loss_total / data_batch
+    correct = correct / data_total_size
+
+    return round(correct, 3), round(loss_avg, 3)
 
 
 # 定义测试函数
@@ -107,7 +125,7 @@ def test_fun(dataLoader, model, loss_fn):
     model.eval()
     test_loss, correct = 0, 0
 
-    with torch.no_grad(): # 不需要计算梯度
+    with torch.no_grad():  # 不需要计算梯度
         for x, y in dataLoader:
             x, y = x.to(device), y.to(device)
             pred = model(x)
@@ -118,17 +136,39 @@ def test_fun(dataLoader, model, loss_fn):
 
     print(f"Test Result:\n Accuracy:{(100 * correct):>0.1f}%, Avg loss:{test_loss:>8f} \n")
 
-
+train_acc_list = []
+train_loss_list = []
 
 if __name__ == '__main__':
     # 定义循环次数，每次循环里面，先训练，再测试
     epochs = 5
     for t in range(epochs):
         print(f"Epoch {t + 1}\n-------------------------------")
-        train_fun(train_dataLoader, model, loss_fn, optimizer)
+        train_acc,train_loss  = train_fun(train_dataLoader, model, loss_fn, optimizer)
+        train_acc_list.append(train_acc_list)
+        train_loss_list.append(train_loss)
+
         test_fun(test_dataLoader, model, loss_fn)
     print("Done!")
 
     # 保存
-    torch.save(model.state_dict(),'model.pth')
+    torch.save(model.state_dict(), 'model.pth')
     print("保存成功")
+
+
+import matplotlib.pyplot as plt
+
+x_list = [i+1 for i in range(len(train_acc_list))]
+plt.plot(x_list, train_acc_list, label="Train")
+plt.title("Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
+
+plt.plot(x_list, train_loss_list, label="Train")
+plt.title("Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
---
+++ A04_imooc_pytorch/A04_pytorch/03_demo_test.py
@@ -0,0 +1,193 @@
+import torch
+import matplotlib.pyplot as plt
+from sympy.physics.units import current
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from tqdm import tqdm
+
+'''
+训练函数的模版:
+数据集、数据加载器、模型、损失函数、优化器、数据保存
+'''
+
+# 数据集
+train_data = datasets.FashionMNIST(root='data', train=True, download=True, transform=ToTensor())
+test_data = datasets.FashionMNIST(root='data', train=False, download=True, transform=ToTensor())
+
+# 数据加载器
+batch_size = 64
+train_dataLoader = DataLoader(train_data, batch_size=batch_size)
+test_dataLoader = DataLoader(test_data, batch_size=batch_size)
+
+# 打印数据的格式，便于书写模型
+print("train_dataLoader-come")
+
+for x, y in train_dataLoader:
+    # Shape of X [N, C, H, W]: torch.Size([64, 1, 28, 28])
+    print(f"Shape of X [N, C, H, W]: {x.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+print("test_dataLoader-come")
+for x, y in test_dataLoader:
+    # Shape of X [N, C, H, W]: torch.Size([64, 1, 28, 28])
+    print(f"Shape of X [N, C, H, W]: {x.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+# 模型
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class NeuralNetWork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # 将输入张量展平，便于输入全连接层和线性层
+        self.flatten = nn.Flatten()
+        self.liner_relu_stack = nn.Sequential(
+            # 28*28是print(f"Shape...)拿到的数据集尺寸大小
+            # 512是自己定义的模型输出特征数
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.liner_relu_stack(x)
+        return x
+
+
+model = NeuralNetWork().to(device)
+print(model)
+
+# 损失函数，CrossEntropyLoss=交叉熵损失函数，常用语分类任务
+# 如果 loss_fn 是交叉熵损失（例如 torch.nn.CrossEntropyLoss），它通常会产生较大的数值，尤其是在模型刚开始训练时，分类错误较多的情况下。
+# 如果 loss_fn 是均方误差损失（例如 torch.nn.MSELoss），并且输出和目标值的差距较大时，损失值也会很大。
+loss_fn = nn.CrossEntropyLoss()
+
+# 优化器，用于更新模型参数，lr=1e-3是学习率
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+
+# 定义训练函数-优化
+def train_fun(dataLoader, model, loss_fn, optimizer):
+    data_total_size = len(dataLoader.dataset)
+    data_batch = len(dataLoader)
+
+    model.train()
+
+    # 总的损失
+    loss_total = 0
+    # 预测正确的个数
+    correct = 0
+    for x, y in tqdm(dataLoader):
+        x, y = x.to(device), y.to(device)
+
+        '''
+        预测结果:tensor([[ 2.0645e-02, -1.0214e-01,  7.6782e-02, -1.2014e-02,  1.3320e-01,
+         -1.8308e-01,  1.6507e-02, -1.1457e-01,  2.1594e-01,  1.5671e-02],
+        [-3.9951e-03, -2.3991e-02,  2.6100e-02,  1.5857e-02,  3.9585e-02,
+        '''
+        pred = model(x)
+
+        '''
+        loss函数结果:2.235142230987549
+        '''
+        loss = loss_fn(pred, y)
+        loss_total += loss.item()
+
+        '''
+        pred.argmax(1) == y的结果:tensor([ True, False, False, False, False, False,  True, False,  True, False,
+         True,  True, False, False,  True, False,  True, False, False, False,
+        False, False, False, False, False, False, False, False,  True, False,
+        '''
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+    loss_avg = loss_total / data_batch
+    correct = correct / data_total_size
+
+    return round(correct, 3), round(loss_avg, 3)
+
+
+# 定义测试函数
+def test_fun(dataLoader, model, loss_fn):
+    data_total_size = len(dataLoader.dataset)
+    data_batch_size = len(dataLoader)
+
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    # 测试集，不需要进行梯度计算 = 提高计算效率并减少内存使用
+    with torch.no_grad():
+        for x, y in tqdm(dataLoader):
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+
+            loss = loss_fn(pred, y)
+
+            test_loss += loss.item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= data_batch_size
+    correct /= data_total_size
+
+    return round(correct, 3), round(test_loss, 3)
+
+
+train_acc_list = []
+train_loss_list = []
+
+
+test_acc_list = []
+test_loss_list = []
+
+
+# 定义循环次数，每次循环里面，先训练，再测试
+if __name__ == '__main__':
+    epochs = 5
+    for t in range(epochs):
+        print(f"\n Epoch {t + 1}\n-------------------------------")
+        # 训练集结果
+        train_acc, train_loss = train_fun(train_dataLoader, model, loss_fn, optimizer)
+        train_acc_list.append(train_acc)
+        train_loss_list.append(train_loss)
+
+        # 测试集结果
+        test_acc,test_loss = test_fun(test_dataLoader, model, loss_fn)
+        test_acc_list.append(test_acc)
+        test_loss_list.append(test_loss)
+
+    print("Done!")
+
+    # 保存
+    # torch.save(model.state_dict(), 'model.pth')
+    # print("保存成功")
+
+    x_list = [i + 1 for i in range(len(train_acc_list))]
+    plt.plot(x_list, train_acc_list, label="Train")
+    plt.plot(x_list, test_acc_list, label="Test")
+    plt.title("Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()
+
+    plt.plot(x_list, train_loss_list, label="Train")
+    plt.plot(x_list, test_loss_list, label="Test")
+
+    plt.title("Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
